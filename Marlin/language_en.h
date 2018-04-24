c1e14d6db7f032e1d3e5eb2b9132624370955207
InsanityAutomation <d.menzel@insanityautomation.com>
2018-04-24 09:32:40 -0400

    Change instance of Light to LED

--- Marlin/language_en.h
+++ Marlin/language_en.h
@@ -928,7 +928,7 @@
   #define MSG_CASE_LIGHT                      _UxGT("LED")
 #endif
 #ifndef MSG_CASE_LIGHT_BRIGHTNESS
-  #define MSG_CASE_LIGHT_BRIGHTNESS           _UxGT("Light BRIGHTNESS")
+  #define MSG_CASE_LIGHT_BRIGHTNESS           _UxGT("LED BRIGHTNESS")
 #endif
 #if LCD_WIDTH >= 20
   #ifndef MSG_INFO_PRINT_COUNT
