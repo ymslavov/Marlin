a5d61601f766ba8eb47b0a57ac141d719d08de86
InsanityAutomation <d.menzel@insanityautomation.com>
2018-04-27 10:41:57 -0400

    Reduce UBL target bed temp for DC bed

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1481,13 +1481,13 @@
   #define USER_SCRIPT_RETURN  // Return to status screen after a script
 
     #define USER_DESC_1 "UBL Commission Step 1"
-  #define USER_GCODE_1 "M502 \n M500 \n M501 \n M190 S75 \n M104 S225 \n G28 \n G29 P1 \n G29 S1 \n M117 Run Step 2 \n"
+  #define USER_GCODE_1 "M502 \n M500 \n M501 \n M190 S60 \n M104 S225 \n G28 \n G29 P1 \n G29 S1 \n M117 Run Step 2 \n"
  
   #define USER_DESC_2 "UBL Commission Step 2"
   #define USER_GCODE_2 "G29 S1 \n G29 S0 \n G29 F 10.0 \n G29 A \n M500 \n G28 \n G29 L1 \n M109 S225 \n G1 X150 Y 150 \n G1 Z0 \n M117 Set Z Offset \n"
 
   #define USER_DESC_3 "Prep for Z Adjust"
-  #define USER_GCODE_3 "M190 75 \n M104 235 \n G28 \n G29 L1 \n G1 X150 Y 150 \n G1 Z0 \n"
+  #define USER_GCODE_3 "M190 60 \n M104 235 \n G28 \n G29 L1 \n G1 X150 Y 150 \n G1 Z0 \n"
 
    #define USER_DESC_4 "Fill Mesh Points"
   #define USER_GCODE_4 "G29 P3 \n G29 P3 \n G29 P3 \n G29 T \n"
