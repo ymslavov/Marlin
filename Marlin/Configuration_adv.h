5dfec58d7b15afe14de2ab7cc0016a3f834cc549
InsanityAutomation <d.menzel@insanityautomation.com>
2018-05-09 12:45:15 -0400

    Couple tweaks
    Turn on multiple probing, off power save recovery, bump version

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -570,7 +570,7 @@
    * an option on the LCD screen to continue the print from the last-known
    * point in the file.
    */
-  #define POWER_LOSS_RECOVERY
+  //#define POWER_LOSS_RECOVERY
 
 
   /**
--- Marlin/Version.h
+++ Marlin/Version.h
@@ -35,7 +35,7 @@
   /**
    * Marlin release version identifier
    */
-  #define SHORT_BUILD_VERSION "1.1.8-B4"
+  #define SHORT_BUILD_VERSION "1.1.8-B5"
 
   /**
    * Verbose version identifier which should contain a reference to the location
