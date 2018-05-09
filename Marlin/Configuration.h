b018fb59d7d2dac6664b02fc2fcb3029eb7d7e50
InsanityAutomation <d.menzel@insanityautomation.com>
2018-05-09 12:52:26 -0400

    Slow nozzle park to help prevent shifting

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1262,7 +1262,7 @@
 #if ENABLED(NOZZLE_PARK_FEATURE)
   // Specify a park position as { X, Y, Z }
   #define NOZZLE_PARK_POINT { 10, 10, 20}
-  #define NOZZLE_PARK_XY_FEEDRATE 100   // X and Y axes feedrate in mm/s (also used for delta printers Z axis)
+  #define NOZZLE_PARK_XY_FEEDRATE 70   // X and Y axes feedrate in mm/s (also used for delta printers Z axis)
   #define NOZZLE_PARK_Z_FEEDRATE 5      // Z axis feedrate in mm/s (not used for delta printers)
 #endif
 
