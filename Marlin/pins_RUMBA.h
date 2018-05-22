88b9cc0e567075147d925a8565887feeda1910f9
InsanityAutomation <d.menzel@insanityautomation.com>
2018-05-22 13:55:13 -0400

    Update to correct ANALOG pin assignment instead of DIGITAL address for same pin

--- Marlin/pins_RUMBA.h
+++ Marlin/pins_RUMBA.h
@@ -108,7 +108,7 @@
 //
 #define TEMP_0_PIN         13   // Analog Input
 #define TEMP_1_PIN         15   // Analog Input
-#define TEMP_BED_PIN       57   // Analog Input
+#define TEMP_BED_PIN       3   // Analog Input
 
 // SPI for Max6675 or Max31855 Thermocouple
 #if DISABLED(SDSUPPORT)
