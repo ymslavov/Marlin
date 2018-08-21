9833ec54754a22a20273d704146f723cd1f4d990
InsanityAutomation <d.menzel@insanityautomation.com>
2018-08-21 18:04:12 -0400

    Bump build date and version

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -40,7 +40,7 @@
   /**
    * Marlin release version identifier
    */
-  #define SHORT_BUILD_VERSION "bugfix-2.0.x_CR1"
+  #define SHORT_BUILD_VERSION "bugfix-2.0.x_CR2"
 
   /**
    * Verbose version identifier which should contain a reference to the location
@@ -53,7 +53,7 @@
    * here we define this default string as the date where the latest release
    * version was tagged.
    */
-  #define STRING_DISTRIBUTION_DATE "2018-08-11"
+  #define STRING_DISTRIBUTION_DATE "2018-08-21"
 
   /**
    * Required minimum Configuration.h and Configuration_adv.h file versions.
