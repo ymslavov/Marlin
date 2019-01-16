ef4c302898d72384dd8d9e5bafbbb869cd4fd323
InsanityAutomation <d.menzel@insanityautomation.com>
2019-01-16 10:59:26 -0500

    Bump to head

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -40,7 +40,7 @@
   /**
    * Marlin release version identifier
    */
-  #define SHORT_BUILD_VERSION "2.0.x_R7"
+  #define SHORT_BUILD_VERSION "2.0.x_R8"
 
   /**
    * Verbose version identifier which should contain a reference to the location
@@ -53,7 +53,7 @@
    * here we define this default string as the date where the latest release
    * version was tagged.
    */
-  #define STRING_DISTRIBUTION_DATE "2019-01-10"
+  #define STRING_DISTRIBUTION_DATE "2019-01-19"
 
   /**
    * Required minimum Configuration.h and Configuration_adv.h file versions.
